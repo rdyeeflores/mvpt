1e29476fa8f6179bd89a82647d91a01b03dd27aa
Ron Flores <ronald.flores@mail.missouri.edu>
2026-06-04 16:21:31 -0500

    adding data to package

---
+++ R/data-UnfairApprais.R
@@ -0,0 +1,29 @@
+#' Unfairness Appraisal Dataset
+#'
+#' Data from an unpublished study examining relations among
+#' perceived unfairness, anger, rumination, and retaliatory aggression.
+#'
+#' @format A data frame with 7 variables:
+#' \describe{
+#'   \item{unfair1}{Measure of appraised unfairness (item 1).}
+#'   \item{unfair2}{Measure of appraised unfairness (item 2).}
+#'   \item{anger1}{Measure of anger (item 1).}
+#'   \item{anger2}{Measure of anger (item 2).}
+#'   \item{rumi2}{Measure of rumination (item 1).}
+#'   \item{rumi3}{Measure of rumination (item 2).}
+#'   \item{Aggr}{Measure of retaliatory aggression.}
+#' }
+#'
+#' @details
+#' The data is a subset from a larger study, and  intended for 
+#' MVPT demonstration.
+#'
+#' @source Data collected by Ronald D. Yee-Flores.
+#'
+#' @references None. Data have not yet been published.
+#'
+#' @examples
+#' data(UnfairApprais)
+#' str(UnfairApprais)
+#' summary(UnfairApprais)
+"UnfairApprais"
